0d4f62e1bcefd80088f3d7119ccf142357857ddd
TalhaKck <45772300+TalhaKck@users.noreply.github.com>
2021-04-24 21:30:57 +0300

    En

--- assessments/C1M2-KOCAK/src/sources.mk
+++ assessments/C1M2-KOCAK/src/sources.mk
@@ -10,8 +10,30 @@
 #*****************************************************************************
 
 # Add your Source files to this variable
-SOURCES =
+ifeq ($(PLATFORM), MSP432)
 
-# Add your include paths to this variable
-INCLUDES = 
+	SOURCES =  						\
+		main.c 						\
+		memory.c 					\
+		startup_msp432p401r_gcc.c	\
+		system_msp432p401r.c 		\
+		interrupts_msp432p401r_gcc.c
+
+	# Add your include paths to this variable
+	INCLUDES = 					\
+		-I ../include/CMSIS		\
+		-I ../include/common	\
+		-I ../include/msp432
+
+else
+
+	SOURCES =  				\
+		main.c 				\
+		memory.c
+
+	INCLUDES = 				\
+		-I ../include/common	
+
+endif
+
 
